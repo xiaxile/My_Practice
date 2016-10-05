c314f451dfe15c0d12896a91811748a4f073d7e3
xiaxile <xiaxile@outlook.com>
2016-10-05 12:16:22 +0800

    奖学金
    【问题描述】 
    某小学最近得到了一笔赞助，打算拿出其中一部分为学习成绩优秀的前 5名学生发奖学
    金。期末，每个学生都有3门课的成绩：语文、数学、英语。先按总分从高到低排序，如果
    两个同学总分相同，再按语文成绩从高到低排序，如果两个同学总分和语文成绩都相同，那
    么规定学号小的同学排在前面，这样，每个学生的排序是唯一确定的。 
    任务：先根据输入的 3门课的成绩计算总分，然后按上述规则排序，最后按排名顺序输
    出前5名学生的学号和总分。注意，在前5名同学中，每个人的奖学金都不相同，因此，你
    必须严格按上述规则排序。例如，在某个正确答案中，如果前两行的输出数据（每行输出两
    个数：学号、总分）是： 
    7 279 
    5 279 
    这两行数据的含义是：总分最高的两个同学的学号依次是 7号、5号。这两名同学的总
    分都是 279（总分等于输入的语文、数学、英语三科成绩之和），但学号为 7 的学生语文成
    绩更高一些。如果你的前两名的输出数据是： 
    5 279 
    7 279 
    则按输出错误处理，不能得分。 
    【输入】 
    输入文件scholar.in包含n+1行： 
    第 1行为一个正整数 n，表示该校参加评选的学生人数。 
    第 2到n+1行，每行有 3个用空格隔开的数字，每个数字都在0到 100之间。第 j行的
    3 个数字依次表示学号为 j-1 的学生的语文、数学、英语的成绩。每个学生的学号按照输入 
    顺序编号为1~n（恰好是输入数据的行号减 1）。 
      所给的数据都是正确的，不必检验。 
    【输出】 
    输出文件 scholar.out 共有 5 行，每行是两个用空格隔开的正整数, 依次表示前 5
    名学生的学号和总分。

---
+++ 奖学金.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+#include<cstring>
+using namespace std;
+struct student
+{
+	int sun;
+	int chi;
+	int num;
+};
+int main()
+{
+	int n,a,b,c;
+	student stu[7],t;
+	cin>>n;
+	for(int i=1;i<=n;++i)
+	{
+		cin>>a>>b>>c;
+		stu[i].sun=a+b+c;
+		stu[i].chi=a;
+		stu[i].num=i;
+	}
+	for(int i=1;i<n;++i)
+		for(int j=i+1;j<=n;++j)
+			if(stu[i].sun<stu[j].sun)
+				t=stu[i]; stu[i]=stu[j]; stu[j]=t;
+	for(int i=1;i<n;++i)
+		for(int j=i+1;j<=n;++j)
+			if(stu[i].sun==stu[j].sun&&stu[i].chi<stu[j].chi)
+				t=stu[i]; stu[i]=stu[j]; stu[j]=t;
+	for(int i=1;i<=5;++i)
+		cout<<stu[i].num<<" "<<stu[i].sun<<endl;
+	return 0;
+}
